61965a0bdb5c7c323accf1b6657d441915246e3f
micfin <michael.finneran@generalassemb.ly>
2019-02-24 16:50:56 +0300

    add exercises and hw

---
+++ exercises/stop-light/index.js
@@ -0,0 +1,23 @@
+function turnOffLights() {
+    // change background color of stop light to red
+}
+// turn on stop function 
+function turnOnStopLight() {
+    // change background color of stop light to red
+}
+
+// turn on slow function
+function turnOnSlowLight() { 
+// change background color of slow light to yellow
+}
+
+// turn on go function
+function turnOnGoLight() {
+    // change background color of go light to green
+}
+
+// set stop light click event
+
+// set slow light click event
+
+// set go light click event
---
+++ exercises/theme-switcher/index.js
@@ -0,0 +1,13 @@
+// click event for gray button
+
+// click event for white button
+
+function switchGray() {
+    // change background to gray
+    // change font to white
+}
+
+function switchWhite() {
+    // change background to white
+    // change font to black
+}
